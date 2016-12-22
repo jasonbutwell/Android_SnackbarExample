9ab39d199ef8432d7dd8e7cf95e58834efe65daa
Jason Butwell <jasonbutwell@gmail.com>
2016-12-22 00:58:45 +0000

    Implemented a selectable action within our snackbar that can be tapped and acted upon by adding an action with an onClickListener.

--- app/src/main/java/com/jasonbutwell/snackbarexample/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/snackbarexample/MainActivity.java
@@ -1,9 +1,12 @@
 package com.jasonbutwell.snackbarexample;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +19,35 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_snackbar);
 
-        displayBasicSnackBar();
+//        displayBasicSnackBar();
+        displayActionSnackbar();
+    }
+
+    // Display a snackbar that supports an action
+
+    public void displayActionSnackbar() {
+
+        // Build a new snackbar
+
+        Snackbar snackbar = Snackbar.make((CoordinatorLayout)findViewById(R.id.myCoordinatorLayout), "This is a snackbar", Snackbar.LENGTH_LONG);
+        snackbar.setAction("My action", new View.OnClickListener() {
+
+            // We override the default onclick behaviour with our own action
+
+            @Override
+            public void onClick(View v) {
+                // Shows a simple Toast message when the snackbar is tapped
+                Toast.makeText(getApplicationContext(),"Snackbar has been tapped", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Change the text colour to RED
+
+        snackbar.setActionTextColor(Color.RED);
+
+        // Show the snackbar
+
+        snackbar.show();
     }
 
     // Display a simple snackbar
